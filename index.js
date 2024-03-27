ab95da9340b165a5c82b880d4e08d6b6759fd7fe
xlaoru <sia212007@gmail.com>
2024-03-27 14:45:24 +0200

    v1.0

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require("express"); // npm i express
+const mongoose = require("mongoose"); // npm i mongoose
+
+const Product = require("./models/product.model.js");
+
+const app = express();
+
+app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("Codecamp Free CRUD Course");
+});
+
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/api/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.put("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    const updatedProduct = await Product.findById(id);
+
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+
+    res.status(200).json({ message: "Product deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://sia212007:rGP3EBKWWlaEly74@backenddb.gazegku.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB"
+  )
+  .then(() => {
+    console.log("Connected to DB");
+    app.listen(3000, () => {
+      console.log(`Server is running on port 3000`);
+    });
+  })
+  .catch(() => {
+    console.log("Connection Failed");
+  });
